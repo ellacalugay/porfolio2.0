ac8966e39c1e143e7975c21317d2117afe33e46a
ella calugay <ellacalugay@gmail.com>
2026-06-29 01:20:48 +0800

    adding projects

--- portfolio/src/components/Projects.jsx
+++ portfolio/src/components/Projects.jsx
@@ -3,6 +3,15 @@ import ProjectCard from "./ProjectCard";
 import ProjectModal from "./ProjectModal";
 import "../styles/Projects.css";
 import eightBox from "../assets/8box.png";
+import goEquip from "../assets/goequip.png";
+import buildCredit from "../assets/buildcredit.png";
+import jciWorldCongress from "../assets/jciworldcongress.png";
+import jciManilaOrg from "../assets/jcimanilaorg.png";
+import p3kCars from "../assets/p3kcars.png";
+import iiaP from "../assets/iiap.png";
+import Angelicious from "../assets/angelicious.png";
+import ptsi from "../assets/ptsi.png";
+import hikeSafe from "../assets/hikesafe.png";
 
 const projectsData = [
   {
@@ -16,16 +25,97 @@ const projectsData = [
     liveDemo: "https://8box.solutions/",
   },
   {
-    title: "Meridian — UX Overhaul",
-    image: "assets/project2.jpg",
-    description: "Redesigned user experience of fintech dashboard improving usability and conversion.",
-    category: "UI/UX",
-    technologies: ["React", "Node.js", "Figma"],
-    role: "UX/UI Designer",
-    features: ["User Research", "Wireframing", "Prototyping", "Usability Testing"],
-    liveDemo: "#",
-    githubRepo: "#",
+    title: "GoEquip Website",
+    image: goEquip,
+    description: "Developed company website for IT solutions.",
+    category: "Corporate Website",
+    technologies: ["Figma", "Wordpress"],
+    role: "Web Developer",
+    features: ["SEO-Friendly Structure", "Responsive Design", "Service Showcase Pages", "Contact & Inquiry Forms"],
+    liveDemo: "https://goequip.ph/",
   },
+  {
+    title: "Build Credit ",
+    image: buildCredit,
+    description: "Developed company website for IT solutions.",
+    category: "Corporate Website",
+    technologies: ["Figma", "Wordpress"],
+    role: "Web Developer",
+    features: ["SEO-Friendly Structure", "Responsive Design", "Service Showcase Pages", "Contact & Inquiry Forms"],
+    liveDemo: "https://buildcredit.ph/",
+  },
+  {
+    title: "JCI World Congress Website",
+    image: jciWorldCongress,
+    description: "Developed company website for IT solutions.",
+    category: "Corporate Website",
+    technologies: ["Figma", "Wordpress"],
+    role: "Web Developer",
+    features: ["SEO-Friendly Structure", "Responsive Design", "Service Showcase Pages", "Contact & Inquiry Forms"],
+    liveDemo: "https://jcimanila.org/",
+  },
+  {
+    title: "JCI Manila Org Website",
+    image: jciManilaOrg,
+    description: "Developed company website for IT solutions.",
+    category: "Corporate Website",
+    technologies: ["Figma", "Wordpress"],
+    role: "Web Developer",
+    features: ["SEO-Friendly Structure", "Responsive Design", "Service Showcase Pages", "Contact & Inquiry Forms"],
+    liveDemo: "https://builtdifferent.ph/",
+  },
+  {
+    title: "P3KCars Website",
+    image: p3kCars,
+    description: "Developed company website for IT solutions.",
+    category: "Corporate Website",
+    technologies: ["Figma", "Wordpress"],
+    role: "Web Developer",
+    features: ["SEO-Friendly Structure", "Responsive Design", "Service Showcase Pages", "Contact & Inquiry Forms"],
+    liveDemo: "https://p3kcars.com/",
+  },
+  {
+    title: "The Institute of Internal Auditors Philippines Website",
+    image: iiaP,
+    description: "Developed company website for IT solutions.",
+    category: "Corporate Website",
+    technologies: ["Figma", "Wordpress"],
+    role: "Web Developer",
+    features: ["SEO-Friendly Structure", "Responsive Design", "Service Showcase Pages", "Contact & Inquiry Forms"],
+    liveDemo: "https://iia-p.org/",
+  },
+  {
+    title: "Angelicious Website",
+    image: Angelicious,
+    description: "Developed company website for IT solutions.",
+    category: "Corporate Website",
+    technologies: ["Figma", "Wordpress"],
+    role: "Web Developer",
+    features: ["SEO-Friendly Structure", "Responsive Design", "Service Showcase Pages", "Contact & Inquiry Forms"],
+    liveDemo: "https://angelicious.com/",
+  },  
+  {
+    title: "PTSI Website",
+    image: ptsi,
+    description: "Developed company website for IT solutions.",
+    category: "Corporate Website",
+    technologies: ["Figma", "Wordpress"],
+    role: "Web Developer",
+    features: ["SEO-Friendly Structure", "Responsive Design", "Service Showcase Pages", "Contact & Inquiry Forms"],
+    liveCode: "",
+  }, 
+  {
+    title: "HikeSafe App",
+    image: hikeSafe,
+    description: "Developed company website for IT solutions.",
+    category: "Corporate Website",
+    technologies: ["Figma", "Wordpress"],
+    role: "Web Developer",
+    features: ["SEO-Friendly Structure", "Responsive Design", "Service Showcase Pages", "Contact & Inquiry Forms"],
+    liveCode: "",
+  }, 
+  
+  
 ];
 
 function Projects() {
